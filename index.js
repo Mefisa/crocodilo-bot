1b6a6604bb6e521e630d8ec8a4dd776975a84e34
Tiffany <t28ng@uwaterloo.ca>
2021-04-18 12:44:07 -0400

    text channels are JOKES

--- index.js
+++ index.js
@@ -5,9 +5,17 @@ const discordClient = new Discord.Client();
 
 discordClient.once('ready', () => {
   console.log('Nigel is Online');
+  const channel = discordClient.channels.cache.find(channel => channel.name === 'nigel-lp-stock-exchange');
+  for(let i=0; i<50; i++){
+    setTimeout(()=>channel.send('nigel is a crocodile'), 10000);
+  }
 })
 
+
+
+
 discordClient.login(DISCORD_API_KEY);
+
 
 
 
